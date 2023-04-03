cbb05624c2e2ae7b143846d85c9c412460fef12f
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-04-03 23:32:55 +0400

    Initial commit

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,4 +8,11 @@ public class Tests {
         assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
+
+    @Test
+    public void testConstructorWithParams() {
+        Rational r = new Rational(1, 2);
+        assertEquals("Constructor with parametrs returns wrong value", "1/2", r.toString());
+
+    }
 }
