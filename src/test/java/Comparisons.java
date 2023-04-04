8b1babc92a25a3c53dc47e6459d540d8816ad5f0
Maksim Khrestinin <maksim.khrestinin@gmail.com>
2023-04-04 19:15:29 +0400

    Add final code into all test classes

--- src/test/java/Comparisons.java
+++ src/test/java/Comparisons.java
@@ -9,11 +9,9 @@ public class Comparisons {
     public void testEquals() {
         rational = new Rational(1, 5);
         assertTrue("The comparison result is incorrect", rational.equals(new Rational(1, 5)));
-        assertTrue("The comparison result is incorrect", rational.equals(new Rational(5, 25)));
         rational = new Rational(0, 49);
         assertTrue("The comparison result is incorrect", rational.equals(new Rational(0, 1)));
         rational = new Rational(-7, 3);
-        assertTrue("The comparison result is incorrect", rational.equals(new Rational(-7, 3)));
         assertTrue("The comparison result is incorrect", rational.equals(new Rational(-70, 30)));
     }
 
@@ -24,9 +22,8 @@ public class Comparisons {
         assertTrue("The comparison result is incorrect", rational.less(new Rational(2, 5)));
         rational = new Rational(0, 14);
         assertTrue("The comparison result is incorrect", rational.less(new Rational(1, 1678)));
-        rational = new Rational(-45, 234);
-        assertTrue("The comparison result is incorrect", rational.less(new Rational(17, 234)));
         rational = new Rational(-123, 1);
+        assertTrue("The comparison result is incorrect", rational.less(new Rational(17, 234)));
         assertTrue("The comparison result is incorrect", rational.less(new Rational(0, 2)));
         assertTrue("The comparison result is incorrect", rational.less(new Rational(-123, 5)));
         assertTrue("The comparison result is incorrect", rational.less(new Rational(-67, 1)));
@@ -40,7 +37,8 @@ public class Comparisons {
         assertFalse("The comparison result is incorrect", rational.lessOrEqual(new Rational(-1, 2)));
         rational = new Rational(0, 3);
         assertFalse("The comparison result is incorrect", rational.lessOrEqual(new Rational(-7, 2)));
-        rational = new Rational(-1, 5);
-        assertFalse("The comparison result is incorrect", rational.lessOrEqual(new Rational(-1, 4)));
+        rational = new Rational(-2, 5);
+        assertFalse("The comparison result is incorrect", rational.lessOrEqual(new Rational(-2, 7)));
+        assertFalse("The comparison result is incorrect", rational.lessOrEqual(new Rational(-3, 5)));
     }
 }
--- src/test/java/ElementaryOperations.java
+++ src/test/java/ElementaryOperations.java
@@ -3,4 +3,34 @@ import static org.junit.Assert.*;
 
 public class ElementaryOperations {
 
+    private static Rational rtnl;
+
+    @Test
+    public void testAddition() {
+        rtnl = new Rational(1, 2);
+        assertEquals("Wrong addition result", "1/1", rtnl.plus(new Rational(1, 2)).toString());
+        assertEquals("Wrong addition result", "1/2", rtnl.plus(new Rational(0, 3)).toString());
+        assertEquals("Wrong addition result", "0/1", rtnl.plus(new Rational(-1, 2)).toString());
+        rtnl = new Rational(0, 1);
+        assertEquals("Wrong addition result", "-1/6", rtnl.plus(new Rational(-1, 6)).toString());
+        rtnl = new Rational(-3, 8);
+        assertEquals("Wrong addition result", "-13/24", rtnl.plus(new Rational(-1, 6)).toString());
+    }
+
+    @Test
+    public void testProductOfNumbers() {
+        rtnl = new Rational(1, 4);
+        assertEquals("Wrong product result", "3/20", rtnl.multiply(new Rational(3, 5)).toString());
+        assertEquals("Wrong product result", "-1/4", rtnl.multiply(new Rational(-1, 1)).toString());
+        rtnl = new Rational(-7, 1);
+        assertEquals("Wrong product result", "0/1", rtnl.multiply(new Rational(0, 5)).toString());
+        assertEquals("Wrong product result", "3/4", rtnl.multiply(new Rational(-3, 28)).toString());
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testBoundOfRange() {
+        rtnl = new Rational(1, 4);
+        Rational res = rtnl.divide(new Rational(0, 5));
+    }
+
 }
